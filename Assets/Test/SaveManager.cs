d8905fe7c5734279b67364ce790b37919ff235b5
yamashita_aki <aki2611mn22@gmail.com>
2021-07-20 22:16:58 +0900

    ゲームデータjsonファイル保存処理(オプションデータ保存処理完了)

--- Assets/Test/SaveManager.cs
+++ Assets/Test/SaveManager.cs
@@ -5,7 +5,6 @@ using System;
 using System.IO;
 using UnityEditor;
 
-
 [Serializable]
 public struct StageData {
     public List<string> nameList;
@@ -14,75 +13,149 @@ public struct StageData {
     public List<string> timeList;
 }//StageData
 
+[Serializable]
+public struct OptionData {
+    public int bgmVol;
+    public int seVol;
+    public int resolutionH;
+    public int resolutionW;
+    public bool isFullscreen;
+}//OptionData
+
+/// <summary>
+/// ゲームで使用するデータの管理処理
+/// 更新日時:0720
+/// </summary>
 public static class SaveManager {
-    const string SAVE_FILE_PATH = "saveData.json";
+    const string SAVE_DATA_PATH = "/Assets/Test/";
+
+    const string STAGE_FILE_PATH = SAVE_DATA_PATH+"stageData.json";
     public static StageData stageData;
 
-    public static void SaveStageList(List<List<string>> list) {
+    const string OPTION_FILE_PATH = SAVE_DATA_PATH+"optionData.json";
+    public static OptionData optionData;
+
+    /// <summary>
+    /// ステージデータ更新処理
+    /// </summary>
+    /// <param name="list">データ更新するリスト</param>
+    public static void StageDataUpdate(List<List<string>> list) {
         stageData.nameList = list[0];
         stageData.rankList = list[1];
         stageData.scoreList = list[2];
         stageData.timeList = list[3];
-        Save();
-        Debug.Log("ステージデータの更新");
-    }
 
-    public static void LogOutput() {
-        Debug.Log("LogOutputの開始");
-        for (int i = 0; i < stageData.nameList.Count; i++) {
-            Debug.Log((i + 1).ToString() + "リスト目の表示");
-            Debug.Log(stageData.nameList[i]);
-            Debug.Log(stageData.rankList[i]);
-            Debug.Log(stageData.scoreList[i]);
-            Debug.Log(stageData.timeList[i]);
-        }//for
-    }//LogOutput
+        string jsonData = JsonUtility.ToJson(stageData, true);//SaveData情報
+        DataSave(jsonData, STAGE_FILE_PATH);
+        Debug.Log("ステージデータの更新終了");
+    }//StageDataUpdate
 
-    public static void Save() {
-        
-        string json = JsonUtility.ToJson(stageData, true);//SaveData情報
+    /// <summary>
+    /// オプションデータの更新処理
+    /// </summary>
+    /// <param name="arrayList">データ更新するリスト</param>
+    public static void OptionDataUpdate(ArrayList arrayList) {
+        optionData.bgmVol = (int)arrayList[0];
+        optionData.seVol = (int)arrayList[1];
+        optionData.resolutionH = (int)arrayList[2];
+        optionData.resolutionW = (int)arrayList[3];
+        optionData.isFullscreen = (bool)arrayList[4];
+
+        string jsonData = JsonUtility.ToJson(optionData, true);
+        DataSave(jsonData, OPTION_FILE_PATH);
+        Debug.Log("オプションデータの更新終了");
+    }//OptionDataUpdate
+
+    /// <summary>
+    /// データの保存処理
+    /// </summary>
+    /// <param name="json">保存するjsonData</param>
+    /// <param name="filePath">保存するファイルパス</param>
+    public static void DataSave(string json, string filePath) {
         string path = Directory.GetCurrentDirectory();//プロジェクトフォルダまでのパス
-        path = path + "/Assets/Test/" + SAVE_FILE_PATH;
+        path = path + filePath;
         StreamWriter writer = new StreamWriter(path, false);
         writer.WriteLine(json);
         writer.Flush();
         writer.Close();
-        //LogOutput();
-    }
+    }//DataSave
 
-    public static void Load() {
-        if(File.Exists(Directory.GetCurrentDirectory() + "/Assets/Test/" + SAVE_FILE_PATH)) {
-            Debug.Log("ステージデータの読み込み");
-            string path = Directory.GetCurrentDirectory();
-            FileInfo info = new FileInfo(path + "/Assets/Test/" + SAVE_FILE_PATH);
-            StreamReader reader = new StreamReader(info.OpenRead());
-            string json = reader.ReadToEnd();
-            stageData = JsonUtility.FromJson<StageData>(json);
-            reader.Close();
+    /// <summary>
+    /// 保存データの読み込み処理
+    /// </summary>
+    public static void DataInit() {
+        string path = Directory.GetCurrentDirectory();
+        if (File.Exists(path + OPTION_FILE_PATH)) {
+            Debug.Log("オプションデータの読み込み");
+            DataLoad(OPTION_FILE_PATH);
         } else {
-            stageData = new StageData();
-            StageDataInit();
+            OptionDataGenerate();
         }//if
-    }
+        if (File.Exists(path + STAGE_FILE_PATH)) {//指定のファイルが存在する場合
+            Debug.Log("ステージデータの読み込み");
+            DataLoad(STAGE_FILE_PATH);
+        } else {
+            StageDataGenerate();
+        }//if
+    }//DataLoad
 
-    public static void StageDataInit() {
+    /// <summary>
+    /// データ読み込み処理
+    /// </summary>
+    /// <param name="dataPath">読み込むデータファイルのパス情報</param>
+    public static void DataLoad(string loadFilePath) {
+        FileInfo info = new FileInfo(Directory.GetCurrentDirectory()+loadFilePath);
+        StreamReader reader = new StreamReader(info.OpenRead());
+        string json = reader.ReadToEnd();
+        switch (loadFilePath) {
+            case STAGE_FILE_PATH:
+                stageData = JsonUtility.FromJson<StageData>(json);
+                break;
+            case OPTION_FILE_PATH:
+                optionData = JsonUtility.FromJson<OptionData>(json);
+                break;
+        }//switch
+        reader.Close();
+    }//DataLoad
+
+    /// <summary>
+    /// ステージデータの初期化処理
+    /// </summary>
+    /// 
+    public static void StageDataGenerate() {
         Debug.Log("ステージデータの初期化");
+        stageData = new StageData();
         List<string> nList = new List<string>();
         List<string> rList = new List<string>();
         List<string> sList = new List<string>();
         List<string> tList = new List<string>();
         for (int i = 0; i < 2; i++) {
-            nList.Insert(i, "Stage" + (i+1));
+            nList.Insert(i, "Stage" + (i + 1));
             rList.Insert(i, "E");
             sList.Insert(i, "0");
             tList.Insert(i, "9.59");
-        }
+        }//for
         stageData.nameList = nList;
         stageData.rankList = rList;
         stageData.scoreList = sList;
         stageData.timeList = tList;
-        Save();
+        string jsonData = JsonUtility.ToJson(stageData, true);
+        DataSave(jsonData, STAGE_FILE_PATH);
+    }//StageDataInit
 
-    }
-
-}
+    /// <summary>
+    /// オプションデータの初期化処理
+    /// </summary>
+    /// 
+    public static void OptionDataGenerate() {
+        Debug.Log("オプションデータの初期化");
+        optionData = new OptionData();
+        optionData.bgmVol = 5;
+        optionData.seVol = 5;
+        optionData.resolutionH = 1980;
+        optionData.resolutionW = 1080;
+        optionData.isFullscreen = true;
+        string jsonData = JsonUtility.ToJson(optionData, true);
+        DataSave(jsonData, OPTION_FILE_PATH);
+    }//OptionDataInit
+}//SaveManager
--- Assets/Test/SaveManagerTest.cs
+++ Assets/Test/SaveManagerTest.cs
@@ -5,38 +5,54 @@ using UnityEngine;
 public class SaveManagerTest : MonoBehaviour {
 
 
+    /// <summary>
+    ///ステージデータ
+    /// </summary>
     public List<List<string>> stageData = new List<List<string>>();
-
     public List<string> nameList = new List<string>();
     public List<string> rankList = new List<string>();
     public List<string> scoreList = new List<string>();
     public List<string> timeList = new List<string>();
+    /// <summary>
+    /// オプションデータ
+    /// </summary>
+    public ArrayList optionData = new ArrayList();
 
     // Start is called before the first frame update
     void Start() {
-        SaveManager.Load();
-        nameList.AddRange(SaveManager.stageData.nameList);
-        rankList.AddRange(SaveManager.stageData.rankList);
-        scoreList.AddRange(SaveManager.stageData.scoreList);
-        timeList.AddRange(SaveManager.stageData.timeList);
-        
-       
-        nameList[1] = "すてーじ2";
-        rankList[1] = "A";
-        scoreList[1] = "1000";
-        timeList[1] = "1:25";
-
-
-        stageData.Insert(0, nameList);
-        stageData.Insert(1, rankList);
-        stageData.Insert(2, scoreList);
-        stageData.Insert(3, timeList);
-
-        SaveManager.SaveStageList(stageData);
+        SaveManager.DataInit();
     }
 
     // Update is called once per frame
     void Update() {
+        if (Input.GetKeyDown(KeyCode.F1)) {
 
-    }
-}
+            nameList.AddRange(SaveManager.stageData.nameList);
+            rankList.AddRange(SaveManager.stageData.rankList);
+            scoreList.AddRange(SaveManager.stageData.scoreList);
+            timeList.AddRange(SaveManager.stageData.timeList);
+            Debug.Log(nameList[0].ToString());
+            //Debug.Log(SaveManager.optionData.resolutionH);
+            nameList[1] = "すてーじ2";
+            rankList[1] = "A";
+            scoreList[1] = "1000";
+            timeList[1] = "1:25";
+
+            stageData.Insert(0, nameList);
+            stageData.Insert(1, rankList);
+            stageData.Insert(2, scoreList);
+            stageData.Insert(3, timeList);
+
+            Debug.Log(SaveManager.optionData.bgmVol);
+            optionData.Insert(0,SaveManager.optionData.bgmVol);
+            optionData.Insert(1, SaveManager.optionData.seVol);
+            optionData.Insert(2, SaveManager.optionData.resolutionH);
+            optionData.Insert(3, SaveManager.optionData.resolutionW);
+            optionData.Insert(4, SaveManager.optionData.isFullscreen);
+
+            optionData[0] = (int)8;
+            SaveManager.StageDataUpdate(stageData);
+            SaveManager.OptionDataUpdate(optionData);
+        }//if
+    }//Update
+}//SaveManagerTest

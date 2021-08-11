52382ba474357c5b3382f970a58880c699a0a594
yamashita_aki <aki2611mn22@gmail.com>
2021-08-11 20:01:29 +0900

    切り裂き(ダメージ)処理 フォルダ整理

---
+++ Assets/01_Scripte/012_Effect/SlashingDamage.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 斬撃ダメージ描画処理
+/// 更新日時:0811
+/// </summary>
+public class SlashingDamage : MonoBehaviour {
+
+    private void Update() {
+        if (this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("StateTest")) {
+            Destroy(this.gameObject);
+        }//if
+    }//Update
+}//SlahingDamage

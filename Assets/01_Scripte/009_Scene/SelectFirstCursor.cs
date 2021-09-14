ee8d14bbc39d2281383288965859f720e1baf730
yamashita_aki <aki2611mn22@gmail.com>
2021-09-15 08:23:50 +0900

    ボタン入力処理不具合修正

--- Assets/01_Scripte/009_Scene/SelectFirstCursor.cs
+++ Assets/01_Scripte/009_Scene/SelectFirstCursor.cs
@@ -9,7 +9,24 @@ using UnityEngine.UI;
 /// </summary>
 public class SelectFirstCursor : MonoBehaviour {
 
-    private void Start() {//OnEnableだと反映されない(0408)
-        GetComponent<Selectable>().Select();
-    }//Start
+    private readonly float CURSOR_INPUT_COUNTER = 2;//
+
+    private float _cursorInputCounter;
+
+    private void Update() {
+        CorsorInputCheck();
+    }//Update
+
+    /// <summary>
+    /// カーソルのボタン入力の有効化をチェックする
+    /// </summary>
+    private void CorsorInputCheck() {
+        ///Updateで2回以上回す必要がある
+        ///2回以下だとボタンを押しっぱなしで次シーンに遷移してしまうから。(20210915)
+        if (_cursorInputCounter == CURSOR_INPUT_COUNTER) {
+            GetComponent<Selectable>().Select();
+        }//if
+        _cursorInputCounter++;
+    }//CorsorInputCheck
+
 }//SelectFirstCursor
